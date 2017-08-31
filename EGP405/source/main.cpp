a2cc621ad6eec839ac5553692aecccb3f12c0c78
duncan.carroll <duncan.carroll@mymail.champlain.edu>
2017-08-31 12:17:51 -0400

    part 2 of the tutorial

--- EGP405/source/main.cpp
+++ EGP405/source/main.cpp
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
+#include <iostream>
+#include <string.h>
 #include "RakNet/RakPeerInterface.h"
+#include "RakNet/MessageIdentifiers.h"
 
 unsigned int maxClients;
 unsigned short serverPort;
@@ -10,6 +13,7 @@ int main(void)
 	char str[512];
 	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
 	bool isServer;
+	RakNet::Packet *packet;
 
 	printf("(C) or (S)erver?\n");
 	fgets(str, 512, stdin);
@@ -20,13 +24,81 @@ int main(void)
 		isServer = false;
 	}
 	else {
+		printf("Enter server port\n");
+		fscanf(stdin, "%i", &serverPort);
 		RakNet::SocketDescriptor sd(serverPort, 0);
+
+		printf("Enter max clients\n");
+		fscanf(stdin, "%i", &maxClients);
 		peer->Startup(maxClients, &sd, 1);
 		isServer = true;
 	}
 
+	if (isServer)
+	{
+		printf("Starting the server.\n");
+		// We need to let the server accept incoming connections from the clients
+		peer->SetMaximumIncomingConnections(maxClients);
+	}
+	else {
+		printf("Enter server IP or hit enter for 127.0.0.1\n");
+		//fgets(str, 512, stdin);
+		//if (str[0] == 0) {
+			strcpy(str, "127.0.0.1");
+		//}
+		printf("Starting the client.\n");
+		printf("Enter server port number.\n");
+		fscanf(stdin, "%i", &serverPort);
+		peer->Connect(str, serverPort, 0, 0);
 
-	// TODO - Add code body here
+	}
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				if (isServer) {
+					printf("A client has disconnected.\n");
+				}
+				else {
+					printf("We have been disconnected.\n");
+				}
+				break;
+			case ID_CONNECTION_LOST:
+				if (isServer) {
+					printf("A client lost the connection.\n");
+				}
+				else {
+					printf("Connection lost.\n");
+				}
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
 
 	RakNet::RakPeerInterface::DestroyInstance(peer);
 
